f81f7befcaea8da15ba077cb80120d9c095038d7
prajeshkasodiya-Bytes <prajesh.kasodiya@bytestechnolab.com>
2024-05-22 09:10:46 +0530

    change forgot password screen formet

--- lib/modules/forgot_password/view/forgot_password_screen.dart
+++ lib/modules/forgot_password/view/forgot_password_screen.dart
@@ -33,87 +33,100 @@ class ForgotPasswordScreen extends StatelessWidget {
           return const SizedBox.shrink();
         },
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.symmetric(horizontal: 17),
-        child: BlocBuilder<ForgotPasswordBloc, ForgotPasswordState>(
-          builder: (context, state) {
-            if (state is ForgotPasswordLoading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (state is ForgotPasswordSent) {
-              return SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SmartText(
-                      APPStrings.forgotPassword.tr.interpolate(['']),
-                      style: style.titleTextStyle,
-                    ),
-                    const SizedBox(height: 24),
-                    //  install the buildTools for this version, please download it with SDKManager as hint.
-                    // SmartRichText(
-                    //   textSpans: [
-                    //     TextSpan(
-                    //       text: 'Email has been sent successfully to your email address ',
-                    //       style: TextStyle(color: Colors.black),
-                    //     ),
-                    //     TextSpan(
-                    //       text: 'someone@email.com',
-                    //       style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
-                    //       recognizer: TapGestureRecognizer()
-                    //         ..onTap = () {
-                    //           // Handle email tap
-                    //         },
-                    //     ),
-                    //     TextSpan(
-                    //       text: '. Please use that link to change your password.',
-                    //       style: TextStyle(color: Colors.black),
-                    //     ),
-                    //   ],
-                    //   padding: EdgeInsets.all(16.0),
-                    //   textAlign: TextAlign.center,
-                    // ),
+      body: BlocBuilder<ForgotPasswordBloc, ForgotPasswordState>(
+        builder: (context, state) {
+          if (state is ForgotPasswordInitial) {
+            return buildInitialView(context);
+          } else if (state is ForgotPasswordLoading) {
+            return buildLoadingView();
+          } else if (state is ForgotPasswordSent) {
+            return buildSuccessView(context);
+          } else if (state is ForgotPasswordError) {
+            return Container();
+          } else {
+            return Container();
+          }
+        },
+      ),
+    );
+  }
 
-                    // SmartRichText(
-                    //     text: APPStrings.emailHasBeenSendSuccessfully.tr.interpolate(['patel@kgk.com']),
-                    //     subText:
-                    //         APPStrings.emailHasBeenSendSuccessfully.tr.replaceAll(APPStrings.emailHasBeenSendSuccessfully.tr, "@@@@@")),
-                  ],
-                ),
-              );
-            }
-            return SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SmartText(
-                    APPStrings.forgotPassword.tr.interpolate(['']),
-                    style: style.titleTextStyle,
-                  ),
-                  SmartText(
-                    APPStrings.forgotPasswordDescription.tr,
-                    style: style.subTitleStyle,
-                  ),
-                  const SizedBox(height: 32),
-                  SmartTextField(
-                    lableText: APPStrings.email.tr,
-                    hintText: APPStrings.email.tr,
-                    lableStyle: style.lableStyle,
-                    onEditingComplete: () {
-                      FocusScope.of(context).nextFocus();
-                    },
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
+  Widget buildInitialView(BuildContext context) {
+    final style = AppTheme.of(context).signInScreenStyle;
+    final TextEditingController emailController = TextEditingController();
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SmartText(
+            APPStrings.forgotPassword.tr.interpolate(['']),
+            style: style.titleTextStyle,
+          ),
+          SmartText(
+            APPStrings.forgotPasswordDescription.tr,
+            style: style.subTitleStyle,
+          ),
+          const SizedBox(height: 32),
+          SmartTextField(
+            controller: emailController,
+            lableText: APPStrings.email.tr,
+            hintText: APPStrings.email.tr,
+            lableStyle: style.lableStyle,
+            onEditingComplete: () {
+              FocusScope.of(context).nextFocus();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildLoadingView() {
+    return const Center(child: CircularProgressIndicator());
+  }
+
+  Widget buildSuccessView(BuildContext context) {
+    final style = AppTheme.of(context).signInScreenStyle;
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SmartText(
+            APPStrings.forgotPassword.tr.interpolate(['']),
+            style: style.titleTextStyle,
+          ),
+          const SizedBox(height: 24),
+          //  install the buildTools for this version, please download it with SDKManager as hint.
+          // SmartRichText(
+          //   textSpans: [
+          //     TextSpan(
+          //       text: 'Email has been sent successfully to your email address ',
+          //       style: TextStyle(color: Colors.black),
+          //     ),
+          //     TextSpan(
+          //       text: 'someone@email.com',
+          //       style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
+          //       recognizer: TapGestureRecognizer()
+          //         ..onTap = () {
+          //           // Handle email tap
+          //         },
+          //     ),
+          //     TextSpan(
+          //       text: '. Please use that link to change your password.',
+          //       style: TextStyle(color: Colors.black),
+          //     ),
+          //   ],
+          //   padding: EdgeInsets.all(16.0),
+          //   textAlign: TextAlign.center,
+          // ),
+
+          // SmartRichText(
+          //     text: APPStrings.emailHasBeenSendSuccessfully.tr.interpolate(['patel@kgk.com']),
+          //     subText:
+          //         APPStrings.emailHasBeenSendSuccessfully.tr.replaceAll(APPStrings.emailHasBeenSendSuccessfully.tr, "@@@@@")),
+        ],
       ),
     );
   }
