95b2bc70298b268f2ef9edbcd241f8a257479ffd
Prajesh.Kasodiya <prajesh.kasodiya@bytestechnolab.com>
2024-07-12 19:21:33 +0530

    fixes all sprint 6 changes

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyAppState extends State<MyApp> {
                             child: Container(
                               padding: const EdgeInsets.only(top: 55, right: 50),
                               child: const Banner(
-                                message: "11-Jul-24",
+                                message: "12-Jul-24",
                                 location: BannerLocation.bottomStart,
                               ),
                             ),
