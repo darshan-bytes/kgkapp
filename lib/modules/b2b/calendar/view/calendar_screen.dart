d23ea048c0ae9a3616c4559b789a535e11b7439b
prajesh.kasodiya <prajesh.kasodiya@bytestechnolab.com>
2024-08-20 16:03:57 +0530

    - DIY Progress Indicator Up gradation

--- lib/modules/b2b/calendar/view/calendar_screen.dart
+++ lib/modules/b2b/calendar/view/calendar_screen.dart
@@ -197,7 +197,7 @@ class CalendarScreen extends StatelessWidget {
             selectionDecoration: BoxDecoration(border: Border.all(color: style.primary, width: 1.w)),
             monthViewSettings: const MonthViewSettings(
               appointmentDisplayCount: 3,
-              appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
+              appointmentDisplayMode: MonthAppointmentDisplayMode.indicator,
             ),
             timeSlotViewSettings: const TimeSlotViewSettings(
               startHour: 0,
