9c7b1e25ed79b1bc6ba58e908135d115efe56fa7
Prajesh.Kasodiya <prajesh.kasodiya@bytestechnolab.com>
2025-03-10 11:27:34 +0530

    auction section implement in product details

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,7 @@ class _MyAppState extends State<MyApp> {
       child: Container(
         padding: const EdgeInsets.only(top: 55, right: 50),
         child: const Banner(
-          message: "7-March",
+          message: "10-March",
           location: BannerLocation.bottomStart,
         ),
       ),
