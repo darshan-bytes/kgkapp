107c155d457e9c700673618cad5f70bd2f76ed6a
Darshan Vachhani <darshan.vachhani@bytestechnolab.com>
2025-08-20 13:15:27 +0530

    Refactor: Implement Search Functionality in Company Selection Screen
    - Added search functionality to the company selection screen, allowing users to search for companies by name.
    - Introduced `SearchCompanyListEvent` to handle search queries in `CompanyBloc`.
    - Implemented debouncing for search input to optimize performance.
    - Updated `CompanyBloc` to filter the company list based on the search query.
    - Modified `CompanyScreen` to include a search input field and display the filtered company list.
    - Changed `SelectCompanyListState` to no longer pass index and oldIndex, simplifying state management.
    - Adjusted UI layout in `CompanyScreen` to accommodate the new search field and make the company list scrollable.

--- lib/modules/b2b/landing/landing_modules/company/bloc/company_bloc.dart
+++ lib/modules/b2b/landing/landing_modules/company/bloc/company_bloc.dart
@@ -5,13 +5,25 @@ part 'company_event.dart';
 part 'company_state.dart';
 
 class CompanyBloc extends Bloc<CompanyEvent, CompanyState> {
+  final TextEditingController searchController = TextEditingController();
   CscDetails? selectData;
 
-  List<CscDetails> companyList = [];
+  List<CscDetails> _companyList = [];
+
+  List<CscDetails> get companyList {
+    if (searchController.text.trim().isEmpty) {
+      return _companyList;
+    } else {
+      return _companyList
+          .where((element) => element.cscName?.toLowerCase().contains(searchController.text.trim().toLowerCase()) ?? false)
+          .toList();
+    }
+  }
 
   CompanyBloc() : super(CompanyInitialState()) {
     on<InitialCompanyListEvent>(_onInitialCompanyListEvent);
     on<SelectCompanyListEvent>(_onSelectCompanyListEvent);
+    on<SearchCompanyListEvent>(_onSearchCompanyList, transformer: BlocEventDeBouncer.debounceTransformer());
   }
 
   Future<void> _onInitialCompanyListEvent(InitialCompanyListEvent event, Emitter<CompanyState> emit) async {
@@ -21,29 +33,32 @@ class CompanyBloc extends Bloc<CompanyEvent, CompanyState> {
       UserResponse? userResponse = StorageManager().getUserResponse();
       String? defaultCscCode = userResponse?.defaultCscCode;
       if (defaultCscCode != null) {
-        selectData = companyList.firstWhereOrNull((element) => element.cscCode == defaultCscCode) ?? companyList.firstOrNull;
+        selectData = _companyList.firstWhereOrNull((element) => element.cscCode == defaultCscCode) ?? _companyList.firstOrNull;
         if (selectData != null) {
           await StorageManager().setSelectedCsc(selectData!);
         }
       }
     }
     if (selectData != null) {
-      companyList.remove(selectData);
-      companyList.insert(0, selectData!);
+      _companyList.remove(selectData);
+      _companyList.insert(0, selectData!);
     }
-    if (companyList.isNotEmpty) {
-      emit(CompanyListLoadedState(companyList: companyList, selectedData: selectData));
+    if (_companyList.isNotEmpty) {
+      emit(CompanyListLoadedState(companyList: _companyList, selectedData: selectData));
     }
+
+    searchController.addListener(() {
+      add(SearchCompanyListEvent(searchController.text));
+    });
   }
 
   Future<void> _onSelectCompanyListEvent(SelectCompanyListEvent event, Emitter<CompanyState> emit) async {
     emit(CompanyReloadState());
-    int oldIndex = selectData == null ? -1 : companyList.indexOf(selectData!);
     selectData = companyList[event.index];
     if (selectData != null) {
       await StorageManager().setSelectedCsc(selectData!);
     }
-    emit(SelectCompanyListState(event.index, oldIndex));
+    emit(SelectCompanyListState());
   }
 
   Future<void> loadCompanyList(BuildContext context, Emitter<CompanyState> emit) async {
@@ -53,9 +68,14 @@ class CompanyBloc extends Bloc<CompanyEvent, CompanyState> {
         Utils.showMessage(error.message);
       },
       (companyListData) {
-        companyList = companyListData;
+        _companyList = companyListData;
         emit(CompanyListLoadedState(companyList: companyList, selectedData: selectData));
       },
     );
+  }
+
+  // REF: https://thekgk.atlassian.net/browse/TA-834
+  Future<void> _onSearchCompanyList(SearchCompanyListEvent event, Emitter<CompanyState> emit) async {
+    emit(CompanyListLoadedState(companyList: companyList, selectedData: selectData));
   }
 }
